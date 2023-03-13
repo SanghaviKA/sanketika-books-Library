cf76831692c62d8c46bb1bce696220729d0966bd
SanghaviKA <sanghavi258@gmail.com>
2023-03-13 11:34:45 +0530

    modified the apis

--- my-project/src/students/routes.js
+++ my-project/src/students/routes.js
@@ -1,14 +1,21 @@
 const { Router } = require("express");
+const { config } = require("../configs/config");
 const controllers = require("./controllers");
 const router = Router();
 
-router.get("/read", controllers.ReadRecord);
+// router.get(config.api_paths.read, controllers.ReadRecord);
 
-router.put("/read", controllers.UpdateRecord);
-router.delete("/delete", controllers.DeleteRecord);
-router.post("/students/register", controllers.AddStudentsRecord);
-router.post("/books/register", controllers.AddBooksRecord);
-router.post("/books/assign", controllers.AddBookAssignRecord);
-router.post("/books/search", controllers.searchBook);
+router.get(config.api_paths.readbooks, controllers.readBooksRecord);
+
+router.get(config.api_paths.readstudents, controllers.readStudentsRecord);
+
+router.get(config.api_paths.readassign, controllers.readBookAssignRecord);
+
+router.post(config.api_paths.student_register, controllers.AddStudentsRecord);
+router.post(config.api_paths.book_register, controllers.AddBooksRecord);
+
+router.post(config.api_paths.book_assign, controllers.AddBookAssignRecord);
+
+router.post(config.api_paths.book_search, controllers.searchBook);
 
 module.exports = router;
